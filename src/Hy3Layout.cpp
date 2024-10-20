d91661c12576757422c92d1a55fc36209eb3e9bb
myamusashi <jinxprogilang666@gmail.com>
2024-10-21 05:48:11 +0700

    pluh

--- src/Hy3Layout.cpp
+++ src/Hy3Layout.cpp
@@ -1544,7 +1544,7 @@ void Hy3Layout::applyNodeDataToWindow(Hy3Node* node, bool no_animation) {
 	auto window = node->data.as_window();
 	auto root_node = this->getWorkspaceRootGroup(window->m_pWorkspace);
 
-	PHLMONITOR monitor = nullptr;
+	PHLMONITOR monitor;
 
 	if (node->workspace->m_bIsSpecialWorkspace) {
 		for (auto& m: g_pCompositor->m_vMonitors) {
